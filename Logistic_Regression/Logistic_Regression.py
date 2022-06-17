fa8d701c0e9eb911407409ee5853092550a585c2
Sandesh Katkaam <sandeshkatakam@gmail.com>
2022-06-17 19:20:11 +0530

    Implemented forward prop

--- Logistic_Regression/Logistic_Regression.py
+++ Logistic_Regression/Logistic_Regression.py
@@ -40,3 +40,85 @@ def data_summary(train_set_X, test_set_X, train_set_y, test_set_y):
   print ("train_set_y shape: " + str(train_set_y.shape))
   print ("test_set_x shape: " + str(test_set_X.shape))
   print ("test_set_y shape: " + str(test_set_y.shape))
+
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+
+    ### START CODE HERE ### (≈ 1 line of code)
+    s = 1/(1+np.exp(-z))
+    ### END CODE HERE ###
+    
+    return s
+
+def initialize_with_zeros(dim):
+    """
+    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
+    
+    Argument:
+    dim -- size of the w vector we want (or number of parameters in this case)
+    
+    Returns:
+    w -- initialized vector of shape (dim, 1)
+    b -- initialized scalar (corresponds to the bias)
+    """
+    w = np.zeros((dim,1))
+    b = 0
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+    
+    return w, b
+
+def propagate(w, b, X, Y):
+    """
+    Implement the cost function and its gradient for the propagation explained above
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
+
+    Return:
+    cost -- negative log-likelihood cost for logistic regression
+    dw -- gradient of the loss with respect to w, thus same shape as w
+    db -- gradient of the loss with respect to b, thus same shape as b
+    """
+    
+    m = X.shape[1]
+    
+    # FORWARD PROPAGATION (FROM X TO COST)
+    A = sigmoid(np.dot(w.T,X) + b)              # compute activation
+    cost = np.sum(((- np.log(A))*Y + (-np.log(1-A))*(1-Y)))/m  # compute cost
+
+    # BACKWARD PROPAGATION (TO FIND GRAD)
+    dw = (np.dot(X,(A-Y).T))/m
+    db = (np.sum(A-Y))/m
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost
+
+
+if __name__ == "__main__":
+    # Some tests :
+    print ("sigmoid([0, 2]) = " + str(sigmoid(np.array([0,2]))))
+    dim = 2
+    w, b = initialize_with_zeros(dim)
+    print ("w = " + str(w))
+    print ("b = " + str(b))
