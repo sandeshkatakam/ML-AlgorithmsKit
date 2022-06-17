4320bb5bd9c94b28c6adbba0d85e2efc96fb2716
Sandesh Katkaam <sandeshkatakam@gmail.com>
2022-06-17 20:47:23 +0530

    added something

--- Logistic_Regression/Logistic_Regression.py
+++ Logistic_Regression/Logistic_Regression.py
@@ -32,14 +32,14 @@ def data_summary(train_set_X, test_set_X, train_set_y, test_set_y):
   num_px = train_set_X.shape[1]
 
 
-  print ("Number of training examples: m_train = " + str(m_train))
-  print ("Number of testing examples: m_test = " + str(m_test))
-  print ("Height/Width of each image: num_px = " + str(num_px))
-  print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-  print ("train_set_x shape: " + str(train_set_X.shape))
-  print ("train_set_y shape: " + str(train_set_y.shape))
-  print ("test_set_x shape: " + str(test_set_X.shape))
-  print ("test_set_y shape: " + str(test_set_y.shape))
+  print("Number of training examples: m_train = " + str(m_train))
+  print("Number of testing examples: m_test = " + str(m_test))
+  print("Height/Width of each image: num_px = " + str(num_px))
+  print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+  print("train_set_x shape: " + str(train_set_X.shape))
+  print("train_set_y shape: " + str(train_set_y.shape))
+  print("test_set_x shape: " + str(test_set_X.shape))
+  print("test_set_y shape: " + str(test_set_y.shape))
 
 
 def sigmoid(z):
@@ -108,12 +108,66 @@ def propagate(w, b, X, Y):
     assert(db.dtype == float)
     cost = np.squeeze(cost)
     assert(cost.shape == ())
+    print("something")
     
     grads = {"dw": dw,
              "db": db}
     
     return grads, cost
 
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    """
+    This function optimizes w and b by running a gradient descent algorithm
+    
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
+    num_iterations -- number of iterations of the optimization loop
+    learning_rate -- learning rate of the gradient descent update rule
+    print_cost -- True to print the loss every 100 steps
+    
+    Returns:
+    params -- dictionary containing the weights w and bias b
+    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
+    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
+    
+    """
+    
+    costs = []
+    
+    for i in range(num_iterations):
+        
+        
+        # Cost and gradient calculation (≈ 1-4 lines of code)
+        grads, cost = propagate(w, b, X, Y)
+     
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        # update rule (≈ 2 lines of code)
+       
+        w = w - (learning_rate*dw)
+        b = b - (learning_rate*db)
+  
+        
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+        
+        # Print the cost every 100 training iterations
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
 
 if __name__ == "__main__":
     # Some tests :
@@ -122,3 +176,14 @@ if __name__ == "__main__":
     w, b = initialize_with_zeros(dim)
     print ("w = " + str(w))
     print ("b = " + str(b))
+    w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
+    grads, cost = propagate(w, b, X, Y)
+    print ("dw = " + str(grads["dw"]))
+    print ("db = " + str(grads["db"]))
+    print ("cost = " + str(cost))
+    params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+
+    print ("w = " + str(params["w"]))
+    print ("b = " + str(params["b"]))
+    print ("dw = " + str(grads["dw"]))
+    print ("db = " + str(grads["db"]))
