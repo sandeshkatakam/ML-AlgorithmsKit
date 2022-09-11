03a0fa801ffe1eff1bc41c62ebaf3fb52a8ffcf2
Sandesh Katkaam <sandeshkatakam@gmail.com>
2022-09-11 17:02:21 +0530

    added code from local source file

--- Linear_Regression/Linear_Regression.py
+++ Linear_Regression/Linear_Regression.py
@@ -1,91 +1,110 @@
-# This is code implementation of Cost function.
 import numpy as np
-def compute_cost(X,y,theta):
-  """
-  Calculates the cost function for given data
+import pandas as pd
+import matplotlib.pyplot as plt
 
-  Arguments:
-  X : Input features passed in as vector or matrix(if multiple features)(use numpy)
-  y : Labels or ground truth values passed in as a vector
-  theta : parameters matrix for calculating the hypothesis function h.(should be passed as an array)
+class Linear_Regression:
+   
+        
+  def train(self,alpha=0.05,lam=0.01,n_iter=6000):
+    """  'train' method takes argument as Learning Rate(alpha), Regularisation Constant(lam), and No.of Iterations(n_iter).
+     All of these values have been initialised by a default value, but can be changed when required.
+     J_hist and noi(number of iterations) keeps track of cost along with iteration. """
+
+    self.alpha = alpha
+    self.n_iter = n_iter
+    self.lam = lam 
+    self.J_hist = []
+    self.noi = []
+   
+ 
   
-  Returns:
-  The cost function for all the training examples in dataset
+  def cost(self):
+    """ cost method takes no arguments and returns the mean squared error of our hypothesis. """
+    h = self.X@self.theta
+    return (1/(2*self.m))*np.sum((h-self.y)**2) + (self.lam/(self.m*2))*np.sum(self.theta[1:,0]**2)
+  
+  
+  
+  def fit(self,X,y):
+    """  'fit' takes X_train,y_train as arguments, applies gradient descent algorithm to fit our parameters theta and returns theta. """
+    self.m = X.shape[0]
+    self.n = X.shape[1]
+    self.theta = np.zeros((self.n + 1,1))
+    
+    #  Normalization
+    self.X = self.normalize(X)
+    self.y = y[:,np.newaxis]
 
-  """
-  m = len(y)
-  X = np.array([np.ones((m)), X])
-  print(X.shape)
-  h = theta.T * X # matrix multiplication
-  C=[]
-  C=h-y
-  C=np.square(C)
-  J=np.sum(C)/(2*m)
-  return J
+    # Gradient Descent Algorithm.
+    for i in range(self.n_iter):
+         # Parameter update
+         theta1  = self.theta
+         theta1[0,0] = 0
+         self.theta = self.theta - (self.alpha/self.m) * ((self.X.T @ (self.X @ self.theta - self.y)) + self.lam*theta1)
+         self.J_hist.append(self.cost())
+         self.noi.append(i)
+         if(i==0):
+           print("Initial Cost:",self.cost())
+         if(i==self.n_iter-1):
+           print("Final Cost:",self.cost())  
+  
+  
+  def score(self,X,y):
+    """'score' takes X,y as argument and returns the score of our prediction."""
+    X = self.normalize(X)
+    y = y[:,np.newaxis]
+    y_pre = X@self.theta
+    return  1 - (((y - y_pre)**2).sum() / ((y - y.mean())**2).sum())
+  
+ 
+  def accuracy(self,y_test,y_pred):
+    m = len(y_test)
+    sum1 =0
+    for i in range(m):
+     if(y_test[i]==y_pred[i]):
+      sum1+=1
+    return (sum1/m)*100  
+    
+  
+  def plot_learn(self):
+    """plots the learning curve; cost function vs no.of iterations."""
+    plt.plot(self.noi,self.J_hist)
+    plt.xlabel("Number Of Iterations")
+    plt.ylabel("Cost Function")
+    plt.title("Const Function vs Iteration")
 
+  
+  def test_train_split(self,X,y,size):
+        """Splits the dataset into training set and test set based on the input split fraction.
+         It takes X,y,test_size as arguments and returns X_train,X_test,y_train,y_test"""
+        m_test = int(X.shape[0]*size)
+        X_test = X[0:m_test,:]
+        y_test = y[0:m_test]
+        X_train = X[m_test:,:]
+        y_train = y[m_test:]
+        return X_train,X_test,y_train,y_test   
+ 
 
-# Implementation of Gradient Descent Algorithm
-def gradient_descent(X,y,theta,alpha, num_iters):
-  """
-  Performs the gradient descent for the Data given no of iterations
+  def predict(self,X):
+    """ predicting value of target feature using the trained model.
+          takes X as argument and returns as prediction done by the model.
+      """
+    m = X.shape[0]
+    X = self.normalize(X)
+    y_pred = X@self.theta
+    return y_pred.flatten()
 
-  Arguments:
-  X: Input array of data (for uni-variate linear reg : dim = (m,2))
-  y: labels
-  theta: parameters matrix (for 2 parameters : dim = (2,1))
-  Note: Dimensions of H : (m,1), Dimensions of y : (m,1) (Both should be equal)
-  Returns:
-  Theta values after the minimization process of loss function
-  """
-  J_history = np.zeros(num_iters, 1)
-  m = len(y)
-  for iters in range(num_iters-1):
-    H = np.matmul(X,theta)
-    J = np.square(H-y)
-    K = (X.T) * J
-    K = K/m
-    # Theta update step(parameter update step)
+  # Normalises X
+  def normalize(self,X):
+    m = X.shape[0]
+    for i in range(X.shape[1]):
+        X[:,i] = (X[:,i] - np.mean(X[:,i]))/(np.std(X[:,i]) + np.exp(-9))
+    X = np.hstack((np.ones((m, 1)), X))
+    return X
 
-    theta = theta - ((alpha)*K)
-    J_history[iters] = compute_cost(X, y, theta)
-    return [theta, J_history]
-
-
-def mean_normalization(X):
-    """
-    Performs the feature normalization step for the Input data
-
-    Arguments:
-    X : Array of Input data
-    How is X stacked? Dimensions of X
-    Returns:
-    Mean normalized input data( Mean value of the data will be zero)
-    """
-    X_norm = (X - np.mean(X))/(np.max(X)- np.min(X))
-    return X_norm
-
-def normal_eqn_method(X,y):
-  """
-  Calculates the paramter matrix or theta by using Normal Equation method
-
-  Arguments:
-  X: Input array (features)
-  y: Labels
-
-  Returns:
-  theta : Parameter matrix
-  """
-  theta = np.linalg.inv((X.T)*X) * (X.T) * y
-  return theta
-
-
-
-
-if __name__ == "__main__":
-  # Testing the above functions
-  X = []
-  y = []
-  theta= []
+  #  returns the parameter theta(weight) of the model.
+  def get_params(self):
+      return self.theta
 
   
 
--- Logistic_Regression/Logistic_Regression.py
+++ Logistic_Regression/Logistic_Regression.py
@@ -1,189 +1,107 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
+class Logistic_Regression:
 
-
-def load_dataset(path_to_csv, col_names= []):
-  """
-  Loads the dataset of our choice from the csv data format file
-
-  Arguments:
-  path_to_csv: A string indicating the path to the csv file to be loaded
-  col_names: A list to be passed in for getting the column headings in dataframe. 
-
-  returns:
-  A pandas dataframe for the dataset
-  """
-  if col_names == []:
-    df = pd.read_csv(path_to_csv)
-  else:
-    df = pd.read_csv(path_to_csv, names = col_names)
+      
+  def train(self,alpha = 0.09,lam = 0.01,n_iter = 3500):
+      """'train' method takes argument as Learning Rate(alpha), Regularisation Constant(lam), and No.of Iterations(n_iter).
+       All of these values have been initialised by a default value, but can be changed when required.
+       J_hist and noi(number of iterations) keeps track of cost along with iteration. """
   
-  return df
-
-
-
-
-
-def data_summary(train_set_X, test_set_X, train_set_y, test_set_y):
-  m_train = train_set_X.shape[0]
-  m_test =  test_set_X.shape[0]
-  num_px = train_set_X.shape[1]
-
-
-  print("Number of training examples: m_train = " + str(m_train))
-  print("Number of testing examples: m_test = " + str(m_test))
-  print("Height/Width of each image: num_px = " + str(num_px))
-  print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-  print("train_set_x shape: " + str(train_set_X.shape))
-  print("train_set_y shape: " + str(train_set_y.shape))
-  print("test_set_x shape: " + str(test_set_X.shape))
-  print("test_set_y shape: " + str(test_set_y.shape))
-
-
-def sigmoid(z):
-    """
-    Compute the sigmoid of z
-
-    Arguments:
-    z -- A scalar or numpy array of any size.
-
-    Return:
-    s -- sigmoid(z)
-    """
-
-    ### START CODE HERE ### (≈ 1 line of code)
-    s = 1/(1+np.exp(-z))
-    ### END CODE HERE ###
-    
-    return s
-
-def initialize_with_zeros(dim):
-    """
-    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
-    
-    Argument:
-    dim -- size of the w vector we want (or number of parameters in this case)
-    
-    Returns:
-    w -- initialized vector of shape (dim, 1)
-    b -- initialized scalar (corresponds to the bias)
-    """
-    w = np.zeros((dim,1))
-    b = 0
-
-    assert(w.shape == (dim, 1))
-    assert(isinstance(b, float) or isinstance(b, int))
-    
-    return w, b
-
-def propagate(w, b, X, Y):
-    """
-    Implement the cost function and its gradient for the propagation explained above
-
-    Arguments:
-    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
-    b -- bias, a scalar
-    X -- data of size (num_px * num_px * 3, number of examples)
-    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
-
-    Return:
-    cost -- negative log-likelihood cost for logistic regression
-    dw -- gradient of the loss with respect to w, thus same shape as w
-    db -- gradient of the loss with respect to b, thus same shape as b
-    """
-    
-    m = X.shape[1]
-    
-    # FORWARD PROPAGATION (FROM X TO COST)
-    A = sigmoid(np.dot(w.T,X) + b)              # compute activation
-    cost = np.sum(((- np.log(A))*Y + (-np.log(1-A))*(1-Y)))/m  # compute cost
-
-    # BACKWARD PROPAGATION (TO FIND GRAD)
-    dw = (np.dot(X,(A-Y).T))/m
-    db = (np.sum(A-Y))/m
-
-    assert(dw.shape == w.shape)
-    assert(db.dtype == float)
-    cost = np.squeeze(cost)
-    assert(cost.shape == ())
-    print("something")
-    
-    grads = {"dw": dw,
-             "db": db}
-    
-    return grads, cost
-
-def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
-    """
-    This function optimizes w and b by running a gradient descent algorithm
-    
-    Arguments:
-    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
-    b -- bias, a scalar
-    X -- data of shape (num_px * num_px * 3, number of examples)
-    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
-    num_iterations -- number of iterations of the optimization loop
-    learning_rate -- learning rate of the gradient descent update rule
-    print_cost -- True to print the loss every 100 steps
-    
-    Returns:
-    params -- dictionary containing the weights w and bias b
-    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
-    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
-    
-    """
-    
-    costs = []
-    
-    for i in range(num_iterations):
-        
-        
-        # Cost and gradient calculation (≈ 1-4 lines of code)
-        grads, cost = propagate(w, b, X, Y)
+      self.n_iter = n_iter
+      self.alpha = alpha
+      self.lam = lam
+      self.J_hist = []
+      self.noi = []
+  
+      # definition of sigmoid function.
+  def sig(self,x):
+      return 1/(1+np.exp(-1*x))
+  
+      
+  
+  def fit(self,X,y,k):
+      """'fit' takes X_train,y_train , k(no.of classes) as arguments, applies gradinet descent algorithm to fit our parameter theta."""
+      self.k = k
+      m = X.shape[0]
+      n = X.shape[1]
+      self.theta = np.zeros((self.k,n +1))
+      
+      # Normalization.
+      X= self.normalize(X)
+      # One-vs-all Classifier.
+      y_k = np.zeros((m,self.k))
+      
+      # Converting y_k into one vs All type matrix.
+      for i in  range(m):
+          y_k[i,y[i]] = 1
+      
+      # Gradient Descent Algorithm
+      for i in range(self.n_iter):
+          theta1 = self.theta
+          theta1[:,0:1] = np.zeros((self.k,1))
+          z = X@(self.theta.T)
+          h = self.sig(z)
+          # Parameter Update
+          self.theta = self.theta - (self.alpha/m)*( ((h-y_k).T@X) + self.lam*theta1 )
+          # Cost Function Calculation
+          cost = (-1*np.sum(y_k*np.log(h) + (1-y_k)*np.log(1-h))/(m)) + (np.sum(self.theta[:,1:]**2)*(self.lam)/(2*m))
+          self.J_hist.append(cost)
+          self.noi.append(i)
+          if(i==0):
+            print("Initial Cost:",cost)
+          if(i==self.n_iter-1):
+            print("Final Cost:",cost)
+      return self
+      
+      
+  def predict(self,X):
+      """Predicting value of target feature using trained model.
+        takes X as argument and returns prediction done by the model."""
+      X  = self.normalize(X)
+      s = X@self.theta.T
+      s = self.sig(s)
+      y_pred = s.argmax(axis = 1)
+      return y_pred
+  
+      # Calculates accuracy for our prediction with test example.
+  def accuracy(self,y_test,y_pred):
+      m = len(y_test)
+      sum1 =0
+      for i in range(m):
+          if(y_test[i]==y_pred[i]):
+              sum1+=1
+      return (sum1/m)*100
+  
+      # Normalises X
+  def normalize(self,X):
+      m = X.shape[0]
+      for i in range(X.shape[1]):
+          X[:,i] = (X[:,i] - np.mean(X[:,i]))/(np.std(X[:,i]) + np.exp(-9))
+      X = np.hstack((np.ones((m, 1)), X))
+      return X
+  
+      # returns the parameter 'theta'
+  def get_params(self):
+      return self.theta
+           
+      
+  def test_train_split(self,X,y,size):
+        """Splits the dataset into trainig set and test set based on input split fraction.
+        takes X,y,test_size as arguments and returns X_train,X_test,y_train,y_test."""
+        m_test = int(X.shape[0]*size)
+        X_test = X[0:m_test,:]
+        y_test = y[0:m_test]
+        X_train = X[m_test:,:]
+        y_train = y[m_test:]
+        return X_train,X_test,y_train,y_test   
+  
      
-        # Retrieve derivatives from grads
-        dw = grads["dw"]
-        db = grads["db"]
-        
-        # update rule (≈ 2 lines of code)
-       
-        w = w - (learning_rate*dw)
-        b = b - (learning_rate*db)
-  
-        
-        # Record the costs
-        if i % 100 == 0:
-            costs.append(cost)
-        
-        # Print the cost every 100 training iterations
-        if print_cost and i % 100 == 0:
-            print ("Cost after iteration %i: %f" %(i, cost))
-    
-    params = {"w": w,
-              "b": b}
-    
-    grads = {"dw": dw,
-             "db": db}
-    
-    return params, grads, costs
-
-if __name__ == "__main__":
-    # Some tests :
-    print ("sigmoid([0, 2]) = " + str(sigmoid(np.array([0,2]))))
-    dim = 2
-    w, b = initialize_with_zeros(dim)
-    print ("w = " + str(w))
-    print ("b = " + str(b))
-    w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
-    grads, cost = propagate(w, b, X, Y)
-    print ("dw = " + str(grads["dw"]))
-    print ("db = " + str(grads["db"]))
-    print ("cost = " + str(cost))
-    params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
-
-    print ("w = " + str(params["w"]))
-    print ("b = " + str(params["b"]))
-    print ("dw = " + str(grads["dw"]))
-    print ("db = " + str(grads["db"]))
+  def plot_learn(self):
+      """ Plots the learning curve; cost function vs no.of iterations."""
+      plt.plot(self.noi,self.J_hist)
+      plt.xlabel("Number Of Iterations")
+      plt.ylabel("Cost Function")
+      plt.title("Cost Function vs Iteration")
